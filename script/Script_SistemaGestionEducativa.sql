60ff18b021fe0e3f0dfbccfc5451410c9c76aebc
jesus-cl27 <jcruz2711@outlook.es>
2021-10-04 23:10:58 -0600

    Se finaliza la creacion del script de la base de datos

--- script/Script_SistemaGestionEducativa.sql
+++ script/Script_SistemaGestionEducativa.sql
@@ -4,8 +4,7 @@ go
 --indicamos que vamos a trabajar en esa base de datos
 use  SistemaGestionEducativa
 go
----drop table Estudiante
---go
+
 --creacion de las tablas
 
 --Tabla Usuario
@@ -99,3 +98,98 @@ create table Estudiante
 	FOREIGN KEY (CedulaPadre,NombrePadre) REFERENCES Padre(Cedula,Nombre),    --llaves foraneas
 );
 go
+--tabla Grupo
+--no se permiten nulos
+create table Grupo
+(
+	Codigo int IDENTITY(1,1) not null UNIQUE, --autoincrementar a partir de 1, incrementando en 1
+	Nombre varchar(50) not null,
+	CedulaProfesor bigint not null,
+	NombreProfesor varchar(50) not null,
+	Cupo int not null,
+	Materia varchar(50) not null,
+	Grado int not null,
+	NumeroPeriodo int not null,
+	Anno int not null,
+	PRIMARY KEY (Codigo),   --llave primaria
+	FOREIGN KEY (NumeroPeriodo,Anno) REFERENCES PeriodoLectivo(NumeroPeriodo,Anno),  --llaves foraneas
+	FOREIGN KEY (CedulaProfesor,NombreProfesor) REFERENCES Profesor(Cedula,Nombre),  --llaves foraneas
+	CONSTRAINT chk_Cupo CHECK (Cupo >=0),  --validar que el cupo sea mayor o igual a cero
+	CONSTRAINT chk_Grado CHECK (Grado >=0) --validar que el grado sea mayor o igual a cero
+);
+go
+
+--tabla Matricula
+--no permite nulos
+create table Matricula
+(
+	Codigo int IDENTITY(1000,1) not null UNIQUE,  --autoincrementar a partir de 1000, incrementando en 1
+	CedulaEstudiante bigint not null,
+	NumeroPeriodo int not null,
+	AnnoPeriodo int not null,
+	PRIMARY KEY (Codigo),   --llave primaria
+	FOREIGN KEY (CedulaEstudiante) REFERENCES Estudiante(Cedula),  --llave foranea
+	FOREIGN KEY (NumeroPeriodo,AnnoPeriodo) REFERENCES PeriodoLectivo(NumeroPeriodo,Anno)  --llaves foraneas
+);
+go
+--tabla de Cobros
+--no permite nulos
+create table Cobro
+(
+	Codigo int IDENTITY(10000,1) not null UNIQUE,   --autoincrementar a partir de 10000, incrementando en 1
+	CodigoMatricula int not null,
+	CedulaEstudiante bigint not null,
+	Grado int not null,
+	NumeroPeriodo int not null,
+	AnnoPeriodo int not null,
+	CedulaUsuario bigint not null,
+	MontoTotal int not null,
+	FechaCreacion datetime not null,
+	PRIMARY KEY (Codigo),   --llave primaria
+	FOREIGN KEY (CedulaEstudiante) REFERENCES Estudiante(Cedula),    --llaves foraneas
+	FOREIGN KEY (CodigoMatricula) REFERENCES Matricula(Codigo),    --llaves foraneas
+	FOREIGN KEY (CedulaUsuario) REFERENCES Usuario(Cedula),   --llaves foraneas
+	FOREIGN KEY (NumeroPeriodo,AnnoPeriodo) REFERENCES PeriodoLectivo(NumeroPeriodo,Anno),  --llaves foraneas
+	
+);
+go
+--tabla DetalleCobroMatricula
+--no permite nulos
+create table DetalleCobroMatricula
+(
+	CodigoCobro int not null,
+	Monto int not null,
+	PRIMARY KEY (CodigoCobro),   --llave primaria
+	FOREIGN KEY (CodigoCobro) REFERENCES Cobro(Codigo),  --llave foranea
+	CONSTRAINT chk_MontoMatricula CHECK (Monto >= 0)  --se valida que el monto no sea negativo
+);
+go
+--tabla DetalleCobroPorGrupo
+--no permite nulos
+create table DetalleCobroPorGrupo
+(
+	CodigoCobro int not null,
+	CodigoGrupo int not null,
+	Grado int not null,
+	NumeroPeriodo int not null,
+	AnnoPeriodo int not null,
+	PRIMARY KEY (CodigoCobro,CodigoGrupo),  --llave primaria
+	FOREIGN KEY (CodigoCobro) REFERENCES Cobro(Codigo),  --llave foranea
+	FOREIGN KEY (CodigoGrupo) REFERENCES Grupo(Codigo),   --llave foranea
+	FOREIGN KEY (NumeroPeriodo,AnnoPeriodo) REFERENCES PeriodoLectivo(NumeroPeriodo,Anno), --llaves foraneas
+);
+go
+--tabla CuotasPorGrupo
+--no permite nulos
+create table CuotasPorGrupo
+(
+	CodigoCobro int not null,
+	CodigoGrupo int not null,
+	Consecutivo int not null,
+	Estado varchar(50) not null,
+	Monto int not null,
+	PRIMARY KEY (CodigoCobro,CodigoGrupo,Consecutivo),  --llaves primarias
+	FOREIGN KEY (CodigoCobro,CodigoGrupo) REFERENCES DetalleCobroPorGrupo(CodigoCobro,CodigoGrupo),  --llaves foraneas
+	CONSTRAINT chk_MontoCuota CHECK (Monto >= 0),  --se valida que el monto no pueda ser negativo
+	CONSTRAINT chk_EstadoCuota CHECK (Estado = 'Pagado' or Estado = 'Pendiente')  --se valida que el estado solo pueda ser 'Pagado' o 'Pendiente'
+);
